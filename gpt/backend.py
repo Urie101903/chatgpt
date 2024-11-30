898e1c2e0b6be21d39d9c88781b22592eb440878
Urie101903 <120762638+Urie101903@users.noreply.github.com>
2024-11-30 13:49:56 +0800

    Update backend.py

--- gpt/backend.py
+++ gpt/backend.py
@@ -18,13 +18,8 @@ def GenerateResponse(input_text):
      response = model.generate_content([
      "you are YURI a ChatGPT, so answer all questions as possible",
      "you need to Have a memory on past conversation",
-     "learn from past conversation"
-     "answer all follow up questions"
      "Store previous messages"
-     "Send the entire chat history"
      "user can command you and you need to follow the command",
-     "input: do you have memory?",
-     "output: yes I have memory of past conversations.",
      "input: who are you?",
      "output: AI chatbot",
      "input: what all you can do?",                                 
